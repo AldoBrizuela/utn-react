7036d78e107608aa044bb6d2164ecf0236e40c90
AldoBrizuela <79615784+AldoBrizuela@users.noreply.github.com>
2021-05-25 11:53:37 -0300

    Add files via upload
    TP8 - Funciones Anonimas y Arrow

---
+++ TP8/script.js
@@ -0,0 +1,155 @@
+const productos = [
+    {
+        nombre: "Harina",
+        precio: 35
+    },
+    {
+        nombre: "Pan",
+        precio: 25
+    },
+    {
+        nombre: "Papa",
+        precio: 52
+    },
+    {
+        nombre: "Palta",
+        precio: 55
+    },
+    {
+        nombre: "Fideos",
+        precio: 85
+    },
+    {
+        nombre: "Aceite",
+        precio: 350
+    },
+    {
+        nombre: "Sopa",
+        precio: 86
+    },
+    {
+        nombre: "Mermelada",
+        precio: 108
+    },
+    {
+        nombre: "Porotos",
+        precio: 69
+    },
+    {
+        nombre: "Lentejas",
+        precio: 85
+    },
+    {
+        nombre: "Mandarina",
+        precio: 43
+    },
+    {
+        nombre: "Banana",
+        precio: 79
+    },
+    {
+        nombre: "Leche de almendras",
+        precio: 145
+    },
+    {
+        nombre: "Papel higiénico",
+        precio: 147
+    },
+    {
+        nombre: "Lavandina",
+        precio: 55
+    },
+    {
+        nombre: "Alcohol en gel",
+        precio: 123
+    },
+    {
+        nombre: "Shampoo",
+        precio: 400
+    },
+    {
+        nombre: "Arroz",
+        precio: 66
+    },
+    {
+        nombre: "Harina dos",
+        precio: 35
+    },
+    {
+        nombre: "Salsa de tomate",
+        precio: 35
+    }
+]
+
+let carrito = [];
+const tablacarrito = document.querySelector('#tablaCarrito');
+const totalcompra = document.getElementById('totalcompra');
+
+
+function crearArticulo(producto){
+    var tr = document.createElement('tr');
+    tr.id = "crearProducto";
+
+    var tdNombre = document.createElement('td');
+    var tdNombretexto = document.createTextNode(producto.nombre);
+    tdNombre.appendChild(tdNombretexto);
+
+    var tdPrecio = document.createElement('td');
+    var tdPreciotexto = document.createTextNode(producto.precio);
+    tdPrecio.appendChild(tdPreciotexto);
+
+    var tdBoton = document.createElement('td');
+    var boton = document.createElement('button');
+    var nombreBoton = document.createTextNode("eliminar");
+    boton.appendChild(nombreBoton);
+    boton.onclick = () => { 
+        let i=0;
+        tr.parentNode.removeChild(tr); 
+        i = carrito.findIndex(item =>{return item.nombre==tdNombre.innerHTML;});
+        carrito.splice(i,1);
+        //console.log(i);
+    }
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdPrecio);
+    tr.appendChild(boton);
+
+    return tr;
+    
+}
+
+function ingresaraCarrito(e){
+        
+    if (e.target.checked == true ) {
+           
+            const index = buscarProducto(e.target.name, productos);
+            carrito.push(productos[index]);
+
+            const tr = crearArticulo(productos[index]);
+            tablacarrito.appendChild(tr);
+            e.target.disabled = true;
+    }
+
+}
+
+function buscarProducto(nombreProducto, buscarArray){
+    
+    let index = null;
+    for (let i = 0; i < buscarArray.length; i++) {
+        if (nombreProducto == buscarArray[i].nombre ) {
+            index = i;
+        }  
+    }
+    return index;
+}
+
+function cerrarCompra(){
+    let total=0;
+    for (let i = 0; i < carrito.length; i++) {
+        total += carrito[i].precio;         
+    }
+    console.log(carrito);
+    totalcompra.innerHTML =  "Total de la Compra: "+total;
+    
+}
+
+
